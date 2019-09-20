9d3b2297a6ebb3a9167f7e5c040031f8461184e8
Andrew Creskey <44072237+acreskeyMoz@users.noreply.github.com>
2019-09-20 14:17:34 -0400

    Added responseStart firstPaint perceptualSpeedIndex DOMContentLoaded

--- to_csv_visual_metrics.sh
+++ to_csv_visual_metrics.sh
@@ -1,14 +1,18 @@
 #!/usr/bin/env bash
-for filename in $(find ./data -name *.json); do
+for filename in $(find ./data -name browsertime.json); do
   out_path=$(dirname $filename)/out.csv
+  #echo $out_path
 
   dir="$(dirname $filename)"   # Returns "/from/hear/to"
   dir="$(basename $dir)"  # Returns just "to"
   echo $dir
 
+  #cat $filename | jq --raw-output '.[] | .browserScripts[] | .timings | .pageTimings | [.pageLoadTime] | @csv' > $out_path
+  #cat $filename | jq --arg mode $basename  --raw-output '["mode", "url", "loadtime", "fcp"], (.[] | .info.url as $url | .browserScripts[] | .timings | .timeToContentfulPaint as $fcp | .pageTimings | [$mode, $url, .pageLoadTime, $fcp]) | @csv' > $out_path
+
   # mode is passed into jq as an arg ($dir)
   #capture browserScripts as $metrics, and .visualMetrics as $visualMetrics
   # then we can interate over both arrays simultaneously with the same index
-  cat $filename | jq --arg mode $dir --raw-output '["mode", "url", "loadtime", "firstVisualChange", "speedIndex", "contentfulSpeedIndex", "visualComplete85" ], (.[] | .info.url as $url | .visualMetrics as $visualMetrics | .browserScripts as $metrics | range(0; $visualMetrics | length) as $i | [$mode, $url, $metrics[$i].timings.pageTimings.pageLoadTime, $visualMetrics[$i].FirstVisualChange, $visualMetrics[$i].SpeedIndex, $visualMetrics[$i].ContentfulSpeedIndex, $visualMetrics[$i].VisualComplete85]) | @csv' > $out_path
+  cat $filename | jq --arg mode $dir --raw-output '["mode", "url", "responseStart", "firstPaint", "firstVisualChange", "DOMContentLoaded", "loadtime", "visualComplete85", "speedIndex", "contentfulSpeedIndex", "perceptualSpeedIndex" ], (.[] | .info.url as $url | .visualMetrics as $visualMetrics | .browserScripts as $metrics | range(0; $visualMetrics | length) as $i | [$mode, $url, $metrics[$i].timings.pageTimings.backEndTime, $metrics[$i].timings.firstPaint, $visualMetrics[$i].FirstVisualChange, $metrics[$i].timings.pageTimings.domContentLoadedTime, $metrics[$i].timings.pageTimings.pageLoadTime, $visualMetrics[$i].VisualComplete85, $visualMetrics[$i].SpeedIndex, $visualMetrics[$i].ContentfulSpeedIndex, $visualMetrics[$i].PerceptualSpeedIndex]) | @csv' > $out_path
 
 done
